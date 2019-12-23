b63297cc74a361d4069592f8a849f1b25985c446
tsangclung <1440982358@qq.com>
2019-12-23 17:03:43 +0800

    first commit

---
+++ target/classes/sql/greenwork_standard.sql
@@ -0,0 +1,5 @@
+INSERT INTO greenwork.standard (name, low, hight) VALUES ('humidity', '20', '80');
+INSERT INTO greenwork.standard (name, low, hight) VALUES ('pm10', '10', '20');
+INSERT INTO greenwork.standard (name, low, hight) VALUES ('pm1_0', '10', '20');
+INSERT INTO greenwork.standard (name, low, hight) VALUES ('pm2_5', '20', '115');
+INSERT INTO greenwork.standard (name, low, hight) VALUES ('temperature', '20', '40');
