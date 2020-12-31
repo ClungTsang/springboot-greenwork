f86d8e283751650320c29879dd5ed575045f64ab
HIsheR <shawn1440982358@163.com>
2020-12-31 10:27:35 +0800

    normal update

--- target/classes/sql/greenwork_elefee.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO greenwork.elefee (rangee, fee) VALUES ('high', 1.114);
-INSERT INTO greenwork.elefee (rangee, fee) VALUES ('low', 0.333);
-INSERT INTO greenwork.elefee (rangee, fee) VALUES ('normal', 0.685);
--- target/classes/sql/greenwork_machruntime.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO greenwork.machruntime (machineid, situation, nrgconsumption, frequency, updated, workshopid) VALUES ('ma101', '3', '111', '111', '2019-11-25 00:00:02', 'ws001');
